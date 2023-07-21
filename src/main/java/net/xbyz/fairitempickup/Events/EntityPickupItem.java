868cc99724a4475201fac8389f735f660a894b46
Fruitloopins <fruitloopins@gmail.com>
2023-07-22 08:55:43 +1000

    Add throwerPriority option

--- src/main/java/net/xbyz/fairitempickup/Events/EntityPickupItem.java
+++ src/main/java/net/xbyz/fairitempickup/Events/EntityPickupItem.java
@@ -3,6 +3,7 @@ package net.xbyz.fairitempickup.Events;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +16,12 @@ import java.util.List;
 import java.util.Random;
 
 public class EntityPickupItem implements Listener {
+    private final FileConfiguration config;
+
+    public EntityPickupItem(FileConfiguration config) {
+        this.config = config;
+    }
+
     @EventHandler
     public void onEntityPickupItem(EntityPickupItemEvent event) {
         if (event.getEntity() instanceof Player) {
@@ -25,11 +32,14 @@ public class EntityPickupItem implements Listener {
 
             if (!nearbyPlayers.isEmpty()) {
                 List<Player> nearbyPlayersList = new ArrayList<>(nearbyPlayers);
-
                 Random random = new Random();
-                int randomChoice = random.nextInt(nearbyPlayers.size());
-                Player player = nearbyPlayersList.get(randomChoice);
+                Player player;
 
+                if (config.getBoolean("throwerPriority") && item.getThrower() != null && nearbyPlayersList.contains(Bukkit.getPlayer(item.getThrower()))) {
+                    player = Bukkit.getPlayer(item.getThrower());
+                } else {
+                    player = nearbyPlayersList.get(random.nextInt(nearbyPlayersList.size()));
+                }
                 player.getInventory().addItem(item.getItemStack());
                 player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.2f, 1.6f + random.nextFloat() * (3.4f - 1.6f));
 
--- src/main/java/net/xbyz/fairitempickup/Main.java
+++ src/main/java/net/xbyz/fairitempickup/Main.java
@@ -2,12 +2,18 @@ package net.xbyz.fairitempickup;
 
 import net.xbyz.fairitempickup.Events.EntityPickupItem;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    private final FileConfiguration config = getConfig();
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new EntityPickupItem(), this);
+        config.addDefault("throwerPriority", false);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        Bukkit.getPluginManager().registerEvents(new EntityPickupItem(config), this);
     }
 }
